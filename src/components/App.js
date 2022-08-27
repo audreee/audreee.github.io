a4c0a774e98e37da8be13867c27de7bdd61ee756
Audree Steinberg <audreee@github.com>
2022-08-26 21:50:11 -0700

    Add Welcome and About sections

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+import Welcome from './Welcome';
+import About from './About';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+let Welcome = () => {
+  return (
+    <div className="section-container parent">
+      <div className="text-column">
+        <h1>Hi, I'm Audree!</h1>
+        <p>I'm a full-stack software engineer, currently working at GitHub.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Welcome;
