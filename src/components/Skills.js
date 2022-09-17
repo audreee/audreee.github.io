38e4dbf2d988234cccd6ce9d15a99bac83930128
Audree Steinberg <audreee@github.com>
2022-09-17 16:20:43 -0700

    Styling fixes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,13 +6,13 @@ let Skills = () => {
       <div className="parent section-container">
         <div className="text-column row-container">
           <div className="column">
-            <h1>I enjoy problem-solving</h1>
+            <h2 className="sub-title">I enjoy problem-solving</h2>
             <div>
               <p>I am language agnostic, and I have experience with a variety of languages, frameworks, and tools for front-end and back-end development.</p>
             </div>
           </div>
           <div className="skills column">
-            <h1>Some of my skills</h1>
+            <h2 className="sub-title">Some of my skills</h2>
             <Technologies />
           </div>
         </div>
