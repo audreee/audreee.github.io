8c3b838c29fd19cd5d57e608961df95af9d19f75
Audree Steinberg <audreecodes@gmail.com>
2022-12-15 17:55:41 -0800

    WIP: switch props to sub-component state

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
+import React, { useState, useEffect } from 'react';
 import '../styles/App.css';
 import Welcome from './Welcome';
 import About from './About';
 import Skills from './Skills';
 import Navbar from './Navbar';
-import AppBar from './Appbar';
+// import getWindowDimensions from '../hooks/getWindowDimensions';
 
 function App() {
   return (
     <div className="App parent">
+      <Navbar />
       <div className="sections">
-        <AppBar />
         <Welcome id="welcome" />
         <About id="about" />
         <Skills id="skills"/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,62 +3,69 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Navbar.css';
 
-const Navbar = () => {
+const Navbar = props => {
   const [isMobile, setIsMobile] = useState(false);
-  const [collapse, setCollapse] = useState(false);
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [collapseMenu, setCollapseMenu] = useState(false);
 
-  let checkIsMobile = (e) => {
-    if (isMobile) {
-      setIsMobile(true)
+  // let toggleMobile = () => {
+  //   if (windowWidth < 769) {
+  //     setIsMobile(true);
+  //   } else {
+  //     setIsMobile(false);
+  //   }
+  // };
+
+  let updateWidth = () => {
+    // setWindowWidth(window.innerWidth);
+    if (window.innerWidth < 769) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
     }
-  }
+  };
 
-//   let componentDidUpdate = (prevProps) =>{
-//     if (prevProps.isMobile !== this.props.isMobile) {
-//       this.handleLinkClick();
-//     }
-//   }
-
-//   let componentDidMount = () => {
-//     if (this.props.isMobile) {
-//       this.setState({collapse: true})
-//     }
-//   }
-
-  useEffect(() => {
+  let handleClick = () => {
     if (isMobile) {
-        setCollapse(true)
+      setCollapseMenu(true);
     }
-  }); 
+  };
 
   let renderLinks = () => {
     return(
       <div className="links">
-        <li><a href="#about" onClick={checkIsMobile}>About</a></li>
-        <li><a href="#skills" onClick={checkIsMobile}>Skills</a></li>
+        <li><a href="#about" onClick={handleClick}>About</a></li>
+        <li><a href="#skills" onClick={handleClick}>Skills</a></li>
       </div>
-    )
-  }
-
-    return (
-        <div>
-        <ul id="nav">
-            <div className="nav-left">
-            <li><a href="#" onClick={checkIsMobile}>Audree Steinberg</a></li>
-            {!collapse && !isMobile ? renderLinks() : null}
-            </div>
-            <div className="nav-right">
-            {isMobile ?
-                <div className="burger">
-                <FontAwesomeIcon icon={faBars} size="2x" onClick={setCollapse({ collapse: false})} style={{color: '#F8EFE4'}}/>
-                </div>
-                :
-                renderLinks()
-            }
-            </div>
-        </ul>
-        </div>
     );
-}
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWidth);
+    return () => {
+      window.removeEventListener('resize', updateWidth);
+    };
+    }, []);
+
+  return (
+    <div>
+      <ul id="nav">
+        <div className="nav-left">
+          <li><a href="#welcome" onClick={handleClick}>Audree Steinberg</a></li>
+          {!collapseMenu && !isMobile ? renderLinks() : null}
+        </div>
+        <div className="nav-right">
+          {isMobile ?
+            <div className="burger">
+              <FontAwesomeIcon icon={faBars} size="2x" onClick={setCollapseMenu(false)} style={{ color: '#F8EFE4' }}/>
+            </div>
+            :
+            renderLinks()
+          }
+        </div>
+      </ul>
+    </div>
+  )
+};
 
 export default Navbar;
